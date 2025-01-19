50aa6e489d4019faa4fdd32d72b14c411d87bb45
Lenovo 15 <information@com1software.com>
2025-01-19 16:00:57 -0500

    upadte

--- DLL/main.go
+++ DLL/main.go
@@ -2,10 +2,19 @@ package main
 
 import "C"
 
+// Important Note: There is an export comment line
+// located above each function to make it visible in the DLL.
+// Do not remove or change these comment lines.
+
 //export SayHello
 func SayHello(name *C.char) *C.char {
-	helloMessage := "Hello, " + C.GoString(name) + "!test123"
+	helloMessage := "Hello1, " + C.GoString(name) + "!test123 "
+	helloMessage = SayHelloIn(helloMessage)
 	return C.CString(helloMessage)
+}
+func SayHelloIn(name string) string {
+	helloMessage := "Hello2 from HelloIn, " + name + " Bye"
+	return helloMessage
 }
 
 //export Add
