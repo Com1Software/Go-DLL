8c532be97625856dec05b6c3711feadce5dd07e6
Lenovo 15 <information@com1software.com>
2025-02-20 11:10:15 -0500

    update

--- C#/TestDll/Program.cs
+++ C#/TestDll/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("/Go-DLL/DLL/mydll.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SayHello(string name);
+
+    [DllImport("/Go-DLL/DLL/mydll.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int Add(int a, int b);
+
+    static void Main()
+    {
+        // Call the SayHello function
+        string name = "From C#";
+        IntPtr messagePtr = SayHello(name);
+        string message = Marshal.PtrToStringAnsi(messagePtr);
+        Console.WriteLine(message);
+        Marshal.FreeHGlobal(messagePtr); // Free the memory allocated by the DLL
+
+        // Call the Add function
+        int result = Add(3, 4);
+        Console.WriteLine("Result of Add: " + result);
+    }
+}
